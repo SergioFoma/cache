61f4f2dddfc52a9430a6ed9557db300aa05a1a27
priusf0x <priusfoxlittle@gmail.com>
2026-09-15 03:11:05 +0300

    Minor Update

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,22 @@
 
 int main() {
   try {
-    constexpr size_t kCap = 100;
+    constexpr size_t kCap = 150;
 
     std::vector<int> test = cache::ReadTestsData("tests/test.txt");
+    cache::Config config("config/config.json");
 
+    cache::CacheHierarchy<int, int> cache_hierarchy(
+        config, [](int key) { return key; });
     cache::Belady<int, int> cache_bel(kCap, [](int key) { return key; }, test);
     cache::TwoQueues<int, int> cache_2q(kCap, [](int key) { return key; });
     cache::Lru<int, int> cache_lru(kCap, [](int key) { return key; });
     cache::Lfu<int, int> cache_lfu(kCap, [](int key) { return key; });
     cache::Arc<int, int> cache_arc(kCap, [](int key) { return key; });
     cache::Lirs<int, int> cache_lirs(kCap, [](int key) { return key; });
+
     for (auto key : test) {
+      cache_hierarchy.LookUpUpdate(key);
       cache_bel.LookUpUpdate(key);
       cache_2q.LookUpUpdate(key);
       cache_lru.LookUpUpdate(key);
@@ -33,6 +38,7 @@ int main() {
       cache_lirs.LookUpUpdate(key);
     }
 
+    std::cout << "Cache Hierarchy: " << cache_hierarchy.GetCacheMissCount() << "\n";
     std::cout << "Belady Cache: " << cache_bel.GetCacheMissCount() << '\n';
     std::cout << "2Q: " << cache_2q.GetCacheMissCount() << '\n';
     std::cout << "LRU: " << cache_lru.GetCacheMissCount() << '\n';
